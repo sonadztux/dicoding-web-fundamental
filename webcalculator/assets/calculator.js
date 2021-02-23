2db6c477c8fd669753113209930b9911b089581e
sonadztux <nanda@anakdesa.id>
2021-02-23 21:30:49 +0700

    Implementing Local Storage In Calculator Project

--- webcalculator/assets/calculator.js
+++ webcalculator/assets/calculator.js
@@ -44,6 +44,8 @@ function handleOperator(operator) {
 }
 
 function performCalculation() {
+
+
     if(calculator.firstNumber === null || calculator.operator == null) {
         alert("You don't have pick any operator!");
         return;
@@ -56,7 +58,16 @@ function performCalculation() {
         result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
     }
 
+    const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+    };
+
+    putHistory(history);
     calculator.displayNumber = result;
+    renderHistory();
 }
 
 const buttons = document.querySelectorAll(".button");
