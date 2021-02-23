62568ee7e27f98033f1d5a696fbcc00d3abd3849
sonadztux <nanda@anakdesa.id>
2021-02-23 21:06:41 +0700

    Javascript Basic and DOM Manipulation (Calculator Project)

---
+++ webcalculator/assets/calculator.js
@@ -0,0 +1,93 @@
+const calculator = {
+    displayNumber: "0",
+    operator: null,
+    firstNumber: null,
+    waitingForSecondNumber: false
+
+};
+
+function updateDisplay() {
+    document.querySelector("#displayNumber").innerText = calculator.displayNumber;
+}
+
+function clearCalculator() {
+    calculator.displayNumber = "0";
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.waitingForSecondNumber = false;
+}
+
+function inputDigit(digit) {
+    if(calculator.displayNumber === "0") {
+        calculator.displayNumber = digit;
+    }else{
+        calculator.displayNumber += digit;
+    }
+}
+
+function inverseNumber() {
+    if (calculator.displayNumber !== '0') {
+        calculator.displayNumber = calculator.displayNumber * -1;
+    }
+    return;
+}
+
+function handleOperator(operator) {
+    if (!calculator.waitingForSecondNumber) {
+        calculator.operator = operator;
+        calculator.waitingForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+        calculator.displayNumber = '0';
+    } else {
+        alert('Operator already choosen.')
+    }
+}
+
+function performCalculation() {
+    if(calculator.firstNumber === null || calculator.operator == null) {
+        alert("You don't have pick any operator!");
+        return;
+    }
+
+    let result = 0;
+    if(calculator.operator === "+") {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+    }else {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+    }
+
+    calculator.displayNumber = result;
+}
+
+const buttons = document.querySelectorAll(".button");
+for (let button of buttons) {
+    button.addEventListener("click", function(event) {
+        const target = event.target;
+
+        if(target.classList.contains("clear")) {
+            clearCalculator();
+            updateDisplay();
+            return;
+        }
+
+        if(target.classList.contains('negative')) {
+            inverseNumber();
+            updateDisplay();
+            return;
+        }
+
+        if(target.classList.contains("equal")) {
+            performCalculation();
+            updateDisplay();
+            return;
+        }
+
+        if(target.classList.contains("operator")) {
+            handleOperator(target.innerText);
+            return;
+        }
+
+        inputDigit(target.innerText);
+        updateDisplay();
+    });
+}
