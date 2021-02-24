d0788d29d7c159bda81adef9080be35229d78dfb
sonadztux <nanda@anakdesa.id>
2021-02-24 23:42:55 +0700

    All done one page interface and responsive enough

---
+++ submission/assets/js/script.js
@@ -0,0 +1,15 @@
+const btnDaftar = document.getElementById("daftar");
+const btnDaftarInfluencer = document.getElementById("daftarInfluencer");
+const btnDaftarUMKM = document.getElementById("daftarUMKM");
+
+btnDaftar.addEventListener('click', function (event) {
+    alert("Mohon maaf pendaftaraan user baru saat ini belum dibuka");
+});
+
+btnDaftarInfluencer.addEventListener('click', function (event) {
+    alert("Mohon maaf pendaftaraan influencer baru saat ini belum dibuka");
+});
+
+btnDaftarUMKM.addEventListener('click', function (event) {
+    alert("Mohon maaf pendaftaraan UMKM baru saat ini belum dibuka");
+});
